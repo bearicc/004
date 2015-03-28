26cd82098303b76b6fdd7b9794e199db0e396ce4
bearicc <hxiongustc@gmail.com>
2015-03-28 17:57:29 -0500

    update index style

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'index.html', {'posts': posts})
