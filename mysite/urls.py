ad078a12d3c0f6b96129f29222a82fa9ca2dd288
bearicc <hxiongustc@gmail.com>
2015-03-29 03:24:50 -0500

    update index style

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', 'mysite.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
     url(r'^blog/admin/', include(admin.site.urls)),
+    url(r'^login/$', 'mysite.views.login', name='login'),
+    url(r'^your-name/', 'mysite.views.get_name'),
+    url(r'^thanks/(?P<name>\w+)/$', 'mysite.views.thanks')
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
+from django import forms
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
 
 # Create your views here.
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'index.html', {'posts': posts})
+
+def login(request):
+    current_name = ' '
+    return render(request, 'login.html')#, {'current_name', current_name})
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/'+form.cleaned_data['your_name']+'/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
+def thanks(request, name):
+    return HttpResponse('How are you, '+name+'!')
